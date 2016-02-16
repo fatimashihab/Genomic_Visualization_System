3695755a39686887cbca1e76aacc2df93cdebe4f
fatimashihab <fatima.shihab@hotmail.com>
2016-02-16 15:35:16 +0200

    Update readlengths_plot.py

--- readlengths_plot.py
+++ readlengths_plot.py
@@ -8,12 +8,12 @@ def plot(file):
   position=[]
   levels=[-5]
   found=False
-  
-  
+
+
   for read in file.fetch(until_eof=True):
       end.append(read.reference_end)
       position.append(read.pos)
-      
+
       for m,l in enumerate(levels):
           if read.pos > l:
               i = read.reference_end
@@ -21,26 +21,26 @@ def plot(file):
               found=True
               coeff=1
               break
-  
+
           if not found:
               i= read.reference_end
               levels.append(i)
-      
+
           x=read.pos
           xaxis=[i for i in range (x,i)]
           yaxis=[coeff]*len(xaxis)
           coeff=coeff+2
-  
+
           lines= plt.plot(xaxis,yaxis)
           plt.setp(lines,linewidth=2)
-  
+
   plt.show()
 
 try:
     os.chdir("/home/fatma/Desktop")
     filename=sys.argv[1]
     file= pysam.AlignmentFile(filename, "rb")
-    
+
 except IOError:
     try:
         file_path= raw_input ("Please enter file path: \n")
@@ -52,34 +52,10 @@ except IOError:
     except OSError:
         print("Path or File Name Error.")
     else:
-        position= []
-        number_of_reads=[]
-
-        for pileupcolumn in file.pileup(until_eof=True):
-            number_of_reads.append(pileupcolumn.n)
-            position.append(pileupcolumn.pos)
-
-        #print (number_of_reads)
-        #print ("\n %s" %position)
-
-        plt.plot(position, number_of_reads)
-        plt.show()
-
+        plot(file)
         file.close()
 
 
 else:
-    position= []
-    number_of_reads=[]
-
-    for pileupcolumn in file.pileup(until_eof=True):
-        number_of_reads.append(pileupcolumn.n)
-        position.append(pileupcolumn.pos)
-
-    #print (number_of_reads)
-    #print ("\n %s" %position)
-
-    plt.plot(position, number_of_reads)
-    plt.show()
-
+    plot(file)
     file.close()
